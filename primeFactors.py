81cd63d8f9f8e0bfb82bf9f02bb5d05a73a1568f
Karan <karan.kornguy@gmail.com>
2015-04-25 00:47:21 +0530

    edited primefactors for corner case of n being a prime number

--- primeFactors.py
+++ primeFactors.py
@@ -1,5 +1,5 @@
 isPrime = lambda n: reduce(lambda y,z: y and z, map(lambda c: False if n%c==0 else True, (i for i in range(2,n)))) if n>2 else True
-primeFactors = lambda n: filter(lambda k: n%k==0 and isPrime(k), (x for x in range(2,n)))
+primeFactors = lambda n: filter(lambda k: n%k==0 and isPrime(k), (x for x in range(2,n))) if not isPrime(n) else [n]
 
 n = int(raw_input())
 print primeFactors(n)
